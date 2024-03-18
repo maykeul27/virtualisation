751b86747d10274c8bc959a40ff265cdf4b7c983
Romain <cordierromain44@gmail.com>
2024-03-18 20:54:08 +0100

    add google links

--- Sql/init.sql
+++ Sql/init.sql
@@ -7,9 +7,11 @@ CREATE TABLE IF NOT EXISTS links (
     link VARCHAR(255)
 );
 
-INSERT INTO links (name, link) VALUES (
-                                       ('Minikube git', 'https://github.com/charroux/kubernetes-minikube'),
-                                       ('Minikube git', 'https://github.com/charroux/kubernetes-minikube'),
-                                       ('Service mesh git','https://github.com/charroux/servicemesh'),
-                                       ('Service mesh Kubernetes git', 'https://github.com/charroux/servicemesh-kubernetes')
-                                      );
+INSERT INTO links (name, link) VALUES ('Présentation des ateliers pratiques Google Cloud', 'https://www.cloudskillsboost.google/focuses/2794?locale=fr&parent=catalog'),
+                                      ('Configuring Networks via gcloud', 'https://www.cloudskillsboost.google/focuses/7140?locale=fr&parent=catalog'),
+                                      ('Infrastructure as Code avec Terraform', 'https://www.cloudskillsboost.google/focuses/15842?locale=fr&parent=catalog'),
+                                      ('Déploiement, scaling et mise à jour de votre site Web sur Google Kubernetes Engine', 'https://www.cloudskillsboost.google/focuses/10470?locale=fr&parent=catalog'),
+                                      ('Installation d\'Anthos Service Mesh sur Google Kubernetes Engine', 'https://www.cloudskillsboost.google/focuses/8459?locale=fr&parent=catalog'),
+                                      ('Minikube git', 'https://github.com/charroux/kubernetes-minikube'),
+                                      ('Service mesh git','https://github.com/charroux/servicemesh'),
+                                      ('Service mesh Kubernetes git', 'https://github.com/charroux/servicemesh-kubernetes');
