654d2fbce7c8ccca01dd4f17b59b5195ff0618df
Romain <cordierromain44@gmail.com>
2024-03-20 22:12:41 +0100

    added ingress + modified website-service

--- K3s/apply.sh
+++ K3s/apply.sh
@@ -2,3 +2,4 @@ kubectl apply -f website-deployment.yaml
 kubectl apply -f website-service.yaml
 kubectl apply -f mysql-deployment.yaml
 kubectl apply -f mysql-service.yaml
+kubectl apply -f ingress.yaml
